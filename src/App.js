451678b5d69e3e1258ed69310d6e163760cfa4f2
kkalyankumar9 <kummarikalyan010@gmail.com>
2024-02-21 16:14:54 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SinglePage from './Components/SinglePage';
+
+function App() {
+  return (
+    <div className="App">
+     <SinglePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SinglePage.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import "./SinglePage.css"
+import data from "../db.json"
+import axios from 'axios';
+const SinglePage = () => {
+    const [fetchData,setfetchData]=useState(data)
+console.log(data)
+useEffect(()=>{
+setfetchData()
+
+},[])
+
+
+  return (
+    <>
+    <div class='con'>
+        <div class="header">
+            <h1>Title</h1>
+            <p>Learn xMetal for XML based authoring to manage high volume structured content development and management. This training program is meant for the Technical Writers, Technical Authors, and Information Developers, working on high-end documentation for Automobile, Aerospace, Hardware, and Manufacturing business domains.</p>
+            <button>Read More</button>
+
+        </div>
+        <div class="subHead">
+            <h2>Classes</h2>
+            <p>Learn xMetal for XML based authoring to manage high volume structured content development and management. </p>
+        </div>
+        <div class="displaydata">
+            {
+                data.map((e,i)=>(
+                    <div key={e.ID}>
+                        <img src="https://img.freepik.com/free-photo/cardboard-box-isolated_125540-652.jpg" alt="" />
+                        <h3>{e.Name}</h3>
+                        <p>{e.ShortDesc}</p>
+                        <button>View</button>
+                    </div>
+                ))
+            }
+
+        </div>
+    </div>
+        
+    </>
+  )
+}
+
+export default SinglePage
